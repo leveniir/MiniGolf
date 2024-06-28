3aa27d419005b0317807c296a3b27b1d80977bb2
radoslawtt <132679375+radoslawtt@users.noreply.github.com>
2024-06-28 22:11:31 +0200

    pop

--- Assets/Scripts/DisconnectMessage.cs
+++ Assets/Scripts/DisconnectMessage.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 
 public class DisconnectMessage : MonoBehaviour
 {
+    public  DisconnectMessage Instance;
+    void Start()
+    {
+        Instance = this;
+    }
     public Image disconnectMessage;
    
     public  void ShowDisconnectMessage()
     {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         disconnectMessage.gameObject.SetActive(true);
         Time.timeScale = 0;
     }
